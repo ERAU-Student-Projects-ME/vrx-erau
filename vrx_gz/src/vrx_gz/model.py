ea67e3826a09779ade7e67ad8d75d033dbc74266
dbrobotx <dbrobotx@erau.edu>
2026-08-31 22:36:14 -0400

    Updated default sdf path.

--- vrx_gz/src/vrx_gz/model.py
+++ vrx_gz/src/vrx_gz/model.py
@@ -265,7 +265,7 @@ class Model:
         if not self.urdf:
             if self.model_type in USVS:
                 self.urdf = os.path.join(get_package_share_directory('gazebo_models'),
-                                        'models', 'mini_minion', 'model.sdf')
+                                        'models', 'junior', 'model.sdf')
             elif self.model_type in UAVS:
                 self.urdf = os.path.join(get_package_share_directory('gazebo_models'),
                                                         'models', 'noodle', 'model.sdf')
